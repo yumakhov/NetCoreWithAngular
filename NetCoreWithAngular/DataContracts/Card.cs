2b53b6fb627c7f069af6498bc19e0012ac43e7c7
Yuri Makhov <yu.makhov@gmail.com>
2022-04-03 00:26:11 +0300

    add backend validation

--- NetCoreWithAngular/DataContracts/Card.cs
+++ NetCoreWithAngular/DataContracts/Card.cs
@@ -1,9 +1,16 @@
-﻿namespace NetCoreWithAngular.DataContracts
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetCoreWithAngular.DataContracts
 {
     public class Card
-    {
+    {        
         public Guid Id { get; set; }
+
+        [Required]  
+        [StringLength(200, ErrorMessage = "The {0} value cannot exceed {1} characters. ")]
         public string Name { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public int ItemsCount { get; set; } 
     }
 }
