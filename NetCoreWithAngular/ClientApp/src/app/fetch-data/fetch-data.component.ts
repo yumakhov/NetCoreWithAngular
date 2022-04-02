73234116c98e6d5a9056ef200df57fe2462f8b7e
Yuri Makhov <yu.makhov@gmail.com>
2022-04-02 22:42:49 +0300

    add default values for cards list, fix proxy config

--- NetCoreWithAngular/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ NetCoreWithAngular/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -6,18 +6,17 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './fetch-data.component.html'
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[] = [];
+  public cards: Card[] = [];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
-      this.forecasts = result;
+    http.get<Card[]>(baseUrl + 'cards').subscribe(result => {
+      this.cards = result;
     }, error => console.error(error));
   }
 }
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+interface Card {
+  id: string;
+  name: number;
+  itemsCount: number;
 }
