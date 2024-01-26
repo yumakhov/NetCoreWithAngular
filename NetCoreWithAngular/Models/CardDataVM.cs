cb315d19afdaa7a04d2d7187241863dcf529f821
Yuri Makhov <yu.makhov@gmail.com>
2024-01-26 06:01:48 +0300

    update to net8, add test project

--- NetCoreWithAngular/Models/CardDataVM.cs
+++ NetCoreWithAngular/Models/CardDataVM.cs
@@ -5,7 +5,7 @@ namespace NetCoreWithAngular.Models
     {
         [Required]
         [StringLength(200, ErrorMessage = "The {0} value cannot exceed {1} characters. ")]
-        public string Name { get; set; }
+        public required string Name { get; set; }
 
         [Range(0, int.MaxValue, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public int ItemsCount { get; set; }
